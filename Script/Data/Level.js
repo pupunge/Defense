ca790e09b894e32b31afc8a829c5071c48957f14
pupungeggang@outlook.kr <pupungeggang2@gmail.com>
2022-07-07 01:33:48 +0900

    0.0

--- Script/Data/Level.js
+++ Script/Data/Level.js
@@ -1,10 +1,15 @@
 var dataLevelPosition = [
     0,
-    {'ID' : 1, 'Position' : [], 'Name' : '1-1'}
+    {'ID' : 1, 'Position' : [], 'Name' : '1-1'},
+    {'ID' : 2, 'Position' : [], 'Name' : '1-2'},
+    {'ID' : 3, 'Position' : [], 'Name' : '1-3'},
 ];
 
 var dataLevelField = [
-
+    0,
+    {'ID' : 1, 'Field' : []},
+    {'ID' : 2, 'Field' : []},
+    {'ID' : 3, 'Field' : []}
 ];
 
 var dataLevelEnemy = [
